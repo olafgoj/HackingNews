114d3e6dcf980dafed25cbd4d71f563920a2571e
tehgoju <tehgoju@gmail.com>
2016-11-13 12:51:45 +0100

    Add shell script to remove hashtags and 'Randomowy xkcd...'

---
+++ utils/sed.sh
@@ -0,0 +1,6 @@
+
+# Remove lines with wykop hashtags and 'Randomowy xkcd...'
+for f in *.md; do 
+sed -i '/Randomowy xkcd na dzi.*/d' $f 
+sed -i '/#programowanie #naukaprogramowania #hack.*/d' $f 
+done
